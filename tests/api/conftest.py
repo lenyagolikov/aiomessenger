5d41a6dbadbcc2a299188df09b757414466d12c4
lenyagolikov <lenyagolikov@gmail.com>
2021-10-19 04:26:42 +0300

    Изменены фикстуры

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -1,38 +1,15 @@
 import pytest
 
-from alembic.command import upgrade
-from sqlalchemy import create_engine
-
 from messenger.api.app import create_app
 
 
 @pytest.fixture
-async def migrated_postgres(alembic_config, postgres):
-    """Возвращает URL к БД с примененными миграциями"""
-    upgrade(alembic_config, 'head')
-    return postgres
-
-
-@pytest.fixture
-async def api_client(aiohttp_client):
+async def api_client(aiohttp_client, postgres):
     """Создает клиента для приложения"""
-    app = create_app()
+    app = await create_app(postgres)
     client = await aiohttp_client(app)
 
     try:
         yield client
     finally:
         await client.close()
-
-
-@pytest.fixture
-def migrated_postgres_connection(migrated_postgres):
-    """Соединяется синхронно с БД"""
-    engine = create_engine(migrated_postgres)
-    conn = engine.connect()
-
-    try:
-        yield conn
-    finally:
-        conn.close()
-        engine.dispose()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,31 +1,22 @@
-import os
-from types import SimpleNamespace
-
 import pytest
-from sqlalchemy_utils import create_database, drop_database
-from yarl import URL
+from sqlalchemy.ext.asyncio import create_async_engine
 
-from messenger.utils.db import DB_URL, make_alembic_config
+from messenger.db.models import Base
 
-DB_URL = os.getenv('CI_MESSENGER_DB_URL', 'postgresql' + DB_URL)
+DB_URL = 'postgresql+asyncpg://lenyagolikov:1234@localhost/db_pytest'
 
 
 @pytest.fixture
-def postgres():
-    """Создает временную БД для запуска теста"""
-    db_name = 'db_pytest'
-    db_url = str(URL(DB_URL).with_path(db_name))
-    create_database(db_url)
+async def postgres():
+    """Подготавливает тестовую БД"""
+    engine = create_async_engine(DB_URL)
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
     try:
-        yield db_url
+        yield DB_URL
     finally:
-        drop_database(db_url)
-
-
-@pytest.fixture()
-def alembic_config(postgres):
-    """Создает объект с конфигурацией для alembic, настроенный на временную БД"""
-    cmd_options = SimpleNamespace(config='alembic.ini', name='alembic',
-                                  db_url=postgres, raiseerr=False, x=None)
-    return make_alembic_config(cmd_options)
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+        await engine.dispose()
