bf12796c4ed7f3a08f4d4cbe8464b63c17d83480
lenyagolikov <lenyagolikov@gmail.com>
2021-10-21 17:09:15 +0300

    Добавлены asyncio sleep для удобного дебага таски

--- messenger/api/v1/chats/search_messages_in_chats.py
+++ messenger/api/v1/chats/search_messages_in_chats.py
@@ -167,6 +167,7 @@ async def search_messages_in_chats(async_session, task_id, message, chats):
     Процесс поиска сообщений в чатах для указанной таски
     """
     async with async_session() as session:
+        await asyncio.sleep(10) # чтобы следить статус таска для дебага
         stmt = select(Task).options(selectinload(Task.messages))
         task = await session.execute(stmt.where(Task.task_id == task_id))
         task = task.scalar()
@@ -182,8 +183,8 @@ async def search_messages_in_chats(async_session, task_id, message, chats):
         )
         messages = messages.scalars().all()
         task.messages.extend(messages)
-        
+
+        await asyncio.sleep(10) # чтобы следить статус таска для дебага
         task.status = "SUCCESS"
-        print(task.messages)
         session.add(task)
         await session.commit()
